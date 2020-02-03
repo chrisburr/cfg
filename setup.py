4601628ef55c02dac4084c26f7502766a9307033
Federico Stagni <federico.stagni@cern.ch>
2020-02-03 18:26:35 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cfg-fstagni",
+    name="cfg",
     version="0.0.1",
-    # author="Example Author",
+    author="Federico Stagni",
     # author_email="author@example.com",
-    # description="A small example package",
+    description="DIRAC cfg files reader",
     long_description=long_description,
     long_description_content_type="text/markdown",
     # url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPLv3 License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=2.7',
