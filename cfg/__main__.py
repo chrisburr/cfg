043bbfee12ff446fa27b05068cb2f41b68a7c0c9
Chris Burr <chrisburr@users.noreply.github.com>
2020-02-11 08:59:32 +0100

    Allow version numbers greater than 9

--- cfg/__main__.py
+++ cfg/__main__.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
   res = cfg_o.loadFromFile(sys.argv[1])
 
   if sys.argv[2] == 'latest':
-    release = [x for x in list(res['Releases']) if re.match("v[0-9]r[0-9]", x[0:4])][0]
+    release = [x for x in list(res['Releases']) if re.match(r"v[0-9]+r[0-9]+", x[0:4])][0]
     print(release)
     if len(sys.argv) == 4 and sys.argv[3] == '--print-only':
       exit(0)
